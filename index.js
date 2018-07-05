263ff044f31f1fd92f9d0877d56bbdbbda5faf79
vysokov-mg <vysokov-mg@dartit.ru>
2018-07-05 19:04:32 +0500

    made it better

--- index.js
+++ index.js
@@ -1,36 +1,79 @@
 /**
  * Created by vysokov-mg on 05.07.2018.
  */
+
 'use strict';
-var vm = new Vue ({
+
+
+var vm = new Vue({
+
     el: "#canvas",
+
     data: {
+        meta: {width: 0, height: 0, visible: false},
         x0: 0,
         y0: 0
     },
+
     methods: {
+
+        start: function () {
+            this.meta.visible = true;
+            var canvas = this.getCanvas();
+            canvas.font = "50px Comic Sans MS";
+            canvas.fillStyle = "dodgerblue";
+            canvas.textAlign = "center";
+            canvas.fillText("SELECT SOME AREA", this.meta.width / 2, this.meta.height / 2);
+        },
+
+        getCanvas: function () {
+            return document.getElementById("canvas").getContext("2d");
+        },
+
         onKeyDown: function (event) {
             this.x0 = event.clientX;
             this.y0 = event.clientY;
             console.log(this.x0);
             console.log(this.y0);
         },
+
         onKeyUp: function (event) {
-            var width = Math.abs(event.clientX - this.x0);
-            var height = Math.abs(event.clientY - this.y0);
 
-            console.log(width);
-            console.log(height);
+            var signedWidth = event.clientX - this.x0;
+            var signedHeight = event.clientY - this.y0;
 
-            var canvas = document.getElementById("canvas");
-            var context = canvas.getContext("2d");
             var img = new Image();
-            img.src = "http://placekitten.com/" + width + "/" + height;
+            console.log("http://placekitten.com/" + Math.abs(signedWidth) + "/" + Math.abs(signedHeight));
+            img.src = "http://placekitten.com/" + Math.abs(signedWidth) + "/" + Math.abs(signedHeight);
 
-            img.onload = function () {
-                context.drawImage(img, vm.x0, vm.y0);
-            };
+            img.onload = (function () {
+                this.getCanvas().drawImage(img, this.x0, this.y0, signedWidth, signedHeight);
+            }).bind(this);
         }
 
+    },
+
+    created: function () {
+        this.meta.width = 0.8 * window.innerWidth;
+        this.meta.height = 0.8 * window.innerHeight;
+        this.meta.visible = false;
+        var rect = canvas.getBoundingClientRect();
+    }
+});
+
+var greet = new Vue({
+
+    el: "#greetings",
+
+    data: {
+        meta: {visible: true}
+    },
+
+    methods: {
+
+        onClick: function () {
+            this.meta.visible = false;
+            vm.start();
+        }
     }
 });

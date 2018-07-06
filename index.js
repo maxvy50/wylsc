8f8b716eb2c4931c5b0268eef9a37971b3e06344
vysokov-mg <vysokov-mg@dartit.ru>
2018-07-06 18:40:38 +0500

    made it better

--- index.js
+++ index.js
@@ -4,15 +4,23 @@
 
 'use strict';
 
-
 var vm = new Vue({
 
     el: "#canvas",
 
     data: {
-        meta: {width: 0, height: 0, visible: false},
-        x0: 0,
-        y0: 0
+        meta: {width: 0, height: 0, visible: false, shiftX: 0, shiftY: 0},
+        x0Abs: 0,
+        y0Abs: 0,
+        xAbs: 0,
+        yAbs: 0
+    },
+
+    computed: {
+        x0: this.x0Abs - this.meta.shiftX,
+        y0: this.x0Abs - this.meta.shiftY,
+        x: this.xAbs - this.meta.shiftX,
+        y: this.yAbs - this.meta.shiftY
     },
 
     methods: {
@@ -31,16 +39,23 @@ var vm = new Vue({
         },
 
         onKeyDown: function (event) {
-            this.x0 = event.clientX;
-            this.y0 = event.clientY;
+            this.x0Abs = event.clientX;
+            this.y0Abs = event.clientY;
             console.log(this.x0);
             console.log(this.y0);
         },
 
         onKeyUp: function (event) {
 
-            var signedWidth = event.clientX - this.x0;
-            var signedHeight = event.clientY - this.y0;
+            this.xAbs = event.clientX;
+            this.yAbs = event.clientY;
+
+            console.log(this.x);
+            console.log(this.y);
+
+            var signedWidth = this.x - this.x0;
+            var signedHeight = this.y - this.y0;
+            if (signedWidth * signedHeight === 0) {return;}
 
             var img = new Image();
             console.log("http://placekitten.com/" + Math.abs(signedWidth) + "/" + Math.abs(signedHeight));
@@ -56,8 +71,10 @@ var vm = new Vue({
     created: function () {
         this.meta.width = 0.8 * window.innerWidth;
         this.meta.height = 0.8 * window.innerHeight;
+        var rect = document.getElementById("canvas").getBoundingClientRect();
+        this.meta.shiftX = rect.left;
+        this.meta.shiftY = rect.top;
         this.meta.visible = false;
-        var rect = canvas.getBoundingClientRect();
     }
 });
 
